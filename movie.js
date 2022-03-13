b60eedc01d3d08f38d96edacf34a7892b688c6fd
RaniVaradharaj <88698550+RaniVaradharaj@users.noreply.github.com>
2022-03-13 16:55:47 +0530

    movie

--- movie.js
+++ movie.js
@@ -1,18 +1,18 @@
 class movie{
     constructor(title,studio,rating="PG"){  //parameterized constructor: to call the class
-        this.title = title;
+        this.title = title;  //creating variables to store in the constructor
         this.studio = studio;
         this.rating = rating;
         console.log(`The Title of the movie is ${this.title}, the studio that made the movie is ${this.studio} and The rating is ${this.rating}`);
     }
-getPG(movies,ratings){
-    let pg=[];
-    for(let i=0;i<movies.length;i++){
+getPG(movies,ratings){ //function ot get movies
+    let pg=[]; //empty array to store the movies we got
+    for(let i=0;i<movies.length;i++){ //looping all movies
         if(ratings[i]=="PG"){
-            pg.push(movies[i]);
+            pg.push(movies[i]); //pushing into the array
         }
     }
-    return pg;
+    return pg; //returning to the function
 }
 }
 //a)
